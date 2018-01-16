cdba13d2973f4d43f7e8639b89ad9682a09ab2dc
MaxDistructo <MaxDistructo@users.noreply.github.com>
2018-01-16 09:00:26 -0600

    Start work on first custom plugin

---
+++ src/main/java/maxdistructo/droidbot2/plugin/error404/PluginMain.java
@@ -0,0 +1,40 @@
+package maxdistructo.droidbot2.plugin.error404
+
+public class PluginMain implements IModule{
+
+	private String moduleName = "Error 404 - Name Not Found/Custom Commands";
+	private String moduleVersion = "1.0";
+	private String moduleMinimumVersion = "2.3.0";
+	private String author = "MaxDistructo";
+	public static IDiscordClient client;
+  public static EventDispatcher dispatcher;
+	
+	public void disable() {
+		
+	}
+
+	public boolean enable(IDiscordClient dclient) {
+		client = dclient;
+		dispatcher = client.getDispatcher();
+		dispatcher.registerListener(new MessageHandler());
+		return true;
+	}
+
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public String getMinimumDiscord4JVersion() {
+		return moduleMinimumVersion;
+	}
+
+	public String getName() {
+		return moduleName;
+	}
+
+	public String getVersion() {
+		return moduleVersion;
+	}
+
+}
