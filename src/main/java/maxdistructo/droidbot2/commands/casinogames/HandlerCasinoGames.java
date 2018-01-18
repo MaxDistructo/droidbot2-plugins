741964e5c3bafe640f16d5e1a24c9fbdbbb0addd
MaxDistructo <MaxDistructo@users.noreply.github.com>
2018-01-18 08:39:42 -0600

    Create HandlerCasinoGames.java

---
+++ src/main/java/maxdistructo/droidbot2/commands/casinogames/HandlerCasinoGames.java
@@ -0,0 +1,24 @@
+package maxdistructo.droidbot2.commands.casinogames;
+
+
+public class HandlerCasinoGames{
+
+public static void onMessageRecievedEvent(MessageRecievedEvent event){
+       IMessage message = event.getMessage();
+       IGuild guild = message.getGuild();
+       IUser author = message.getAuthor();
+       IUser mentioned = Utils.getMentionedUser(message);
+       String content = message.getContent();
+       Object messageContent[] = content.split(" ");
+       
+                  if (messageContent[0].equals(prefix + "bj")) { 
+                    message.reply(BlackJack.blackjack(messageContent, message));
+                } else if (messageContent[0].toString().toLowerCase().equals("hit") && Perms.checkGames(message) || messageContent[0].toString().toLowerCase().equals("stay") && Perms.checkGames(message)) {
+                    message.reply(BlackJack.continueGame(message, (String[]) messageContent, CasinoConfig.readBJFields(message)));
+                }
+                
+                
+       }
+
+
+}
