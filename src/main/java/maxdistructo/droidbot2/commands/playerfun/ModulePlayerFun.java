e6242f53afcab82d873b282ac33ea746ee26a228
MaxDistructo <MaxDistructo@users.noreply.github.com>
2018-01-25 07:52:34 -0600

    Update ModulePlayerFun.java

--- src/main/java/maxdistructo/droidbot2/commands/playerfun/ModulePlayerFun.java
+++ src/main/java/maxdistructo/droidbot2/commands/playerfun/ModulePlayerFun.java
@@ -1,1 +1,49 @@
+package maxdistructo.droidbot2.commands.playerfun;
 
+public class ModulePlayerFun implements IModule{
+  
+  
+	private String moduleName = "PlayerFun Commands";
+	private String moduleVersion = "1.0";
+	private String moduleMinimumVersion = "2.9.0";
+	private String author = "MaxDistructo";
+	public static IDiscordClient client;
+  	public static EventDispatcher dispatcher;
+	public static String prefix;
+	
+	public void disable() {
+		
+	}
+
+	public boolean enable(IDiscordClient dclient) {
+		try {
+ 			Class.forName( "maxdistructo.droidbot2.core.ModuleDroidBot2Core" ); //Checks for droidbot2-core to be in classpath
+			prefix = Utils.readPrefix(); //Require config file for prefix
+		} catch( Exception e ) {
+			e.printStackTrace();
+			return false;
+		}
+		client = dclient;
+		dispatcher = client.getDispatcher();
+		dispatcher.registerListener(new MessageHandler());
+		return true;
+	}
+
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public String getMinimumDiscord4JVersion() {
+		return moduleMinimumVersion;
+	}
+
+	public String getName() {
+		return moduleName;
+	}
+
+	public String getVersion() {
+		return moduleVersion;
+	}
+
+}
